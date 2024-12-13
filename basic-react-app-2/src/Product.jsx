926e93727c176f7ac9267d9c3cde7aa86910006e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-14 00:33:19 +0530

    render

---
+++ basic-react-app-2/src/Product.jsx
@@ -0,0 +1,14 @@
+import "./Product.css"
+
+function Product({title, price, features}){
+    return(
+        <div className="Product">
+            <h3>{title}</h3>
+            <h5>Price : {price}</h5>
+            <p>{features}</p>
+            
+        </div>
+    )
+}
+
+export default Product;
