c7710df81b68377eb293958fb3c9a6fac9763401
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-12-14 10:08:04 +0530

    react

--- basic-react-app-3/src/Product.jsx
+++ basic-react-app-3/src/Product.jsx
@@ -3,15 +3,17 @@ import "./Product.css"
 function Product({title, price, features}){
     //rendering arrays
     // const list features.map((feature) => <li>{feature}</li>)
+
+    
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
-            <p>{features}</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : null}
             
         </div>
         
-    )
+    );
 }
 
 export default Product;
